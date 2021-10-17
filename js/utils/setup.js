56cab3cfd05861375030ddb485bb99333cf16a52
SergeySofronov <nekromanser-box@yandex.ru>
2021-10-18 01:37:16 +0500

    7.10. Отрисуй меня полностью (часть 1)

---
+++ js/utils/setup.js
@@ -0,0 +1,67 @@
+import { getRandomAds } from './data.js';
+
+const reworkClonedElement = function (element, elementProperty, propertyValue) {
+  if (propertyValue) {
+    if (element.classList.contains('hidden')) {
+      element.classList.remove('hidden');
+    }
+    element[`${elementProperty}`] = propertyValue;
+  } else {
+    if (!element.classList.contains('hidden')) {
+      element.classList.add('hidden');
+    }
+  }
+};
+
+const getCardsNodes = function () {
+  const userAdsArray = getRandomAds();
+  const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
+  const cardArray = [];
+  userAdsArray.forEach((userAd) => {
+    const cardClone = cardTemplate.cloneNode(true);
+    const popupAvatarElement = cardClone.querySelector('.popup__avatar');
+    const popupTitleElement = cardClone.querySelector('.popup__title');
+    const popupAddressElement = cardClone.querySelector('.popup__text--address');
+    const popupPriceElement = cardClone.querySelector('.popup__text--price');
+    const popupTypeElement = cardClone.querySelector('.popup__type');
+    const popupCapacityElement = cardClone.querySelector('.popup__text--capacity');
+    const popupTimeElement = cardClone.querySelector('.popup__text--time');
+    const popupDescriptionElement = cardClone.querySelector('.popup__description ');
+
+    reworkClonedElement(popupAvatarElement, 'src', userAd.author.avatar);
+    reworkClonedElement(popupTitleElement, 'textContent', userAd.offer.title);
+    reworkClonedElement(popupAddressElement, 'textContent', userAd.offer.address);
+    reworkClonedElement(popupPriceElement, 'textContent', `${userAd.offer.price}₽/ночь`);
+    reworkClonedElement(popupTypeElement, 'textContent', userAd.offer.typeRus);
+    reworkClonedElement(popupCapacityElement, 'textContent', `${userAd.offer.rooms} комнаты для ${userAd.offer.guests} гостей`);
+    reworkClonedElement(popupTimeElement, 'textContent', `Заезд после ${userAd.offer.checkin}, выезд до ${userAd.offer.checkout}`);
+    reworkClonedElement(popupDescriptionElement, 'textContent', userAd.offer.description);
+
+    const popupFeatureElement = cardClone.querySelectorAll('.popup__feature');
+    popupFeatureElement.forEach((nodeItem) => {
+      let isRemoving = true;
+      userAd.offer.features.forEach((arrayItem) => {
+        if (nodeItem.classList.contains(`popup__feature--${arrayItem}`)) {
+          isRemoving = false;
+        }
+      });
+      if (isRemoving) {
+        nodeItem.remove();
+      }
+    });
+
+    const popupPhotoList = cardClone.querySelector('.popup__photos');
+    const photoDefaultElement = cardClone.querySelector('.popup__photo');
+    popupPhotoList.innerHTML = '';
+    userAd.offer.photos.forEach((value, index) => {
+      photoDefaultElement.src = value;
+      photoDefaultElement.alt += ` №${index + 1}`;
+      const photoNewElement = photoDefaultElement.cloneNode(false);
+      popupPhotoList.append(photoNewElement);
+    });
+    cardArray.push(cardClone);
+  });
+  return cardArray;
+};
+
+export { getCardsNodes };
