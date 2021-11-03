3b332f4c20ea897d50bc87f805a5cde194727582
SergeySofronov <nekromanser-box@yandex.ru>
2021-11-03 22:41:30 +0500

    11.13. Надо подкачаться

--- js/main.js
+++ js/main.js
@@ -1,16 +1,11 @@
 import { setFormState } from './utils/form.js';
-import { createMap, createMarkerHeap } from './utils/map.js';
-import { getData } from './utils/fetch.js';
+import { createMap } from './utils/map.js';
 
 const FORM_DISABLE = true;
 const FORM_ENABLE = false;
-//const ERROR_MESSAGE = 'Не удалось загрузить данные с сервера';
 
 // Деактивация формы
 setFormState(FORM_DISABLE);
 
 // Создание карты
 createMap(() => setFormState(FORM_ENABLE));
-
-// Запрос данных с сервера и добавление маркеров на карту
-//getData((markerPopupData) => createMarkerHeap(markerPopupData), console.error);
