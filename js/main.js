8b1ebd862ed92d0c75f66dca7de220d7a2a81821
SergeySofronov <nekromanser-box@yandex.ru>
2021-11-07 23:55:19 +0500

    12.9

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
-import { setFormState } from './utils/form.js';
+import { setFormState, onRoomNumberChange } from './utils/form.js';
 import { createMap } from './utils/map.js';
 
 const FORM_DISABLE = true;
 const FORM_ENABLE = false;
 
+
 // Деактивация формы
 setFormState(FORM_DISABLE);
 
 // Создание карты
-createMap(() => setFormState(FORM_ENABLE));
+createMap(() => {
+  setFormState(FORM_ENABLE);
+  onRoomNumberChange();
+});
